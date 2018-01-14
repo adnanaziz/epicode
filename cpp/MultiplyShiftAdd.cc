904a8cff14601e30669b2f532452b46d636d4c8f
prabodhw96 <prabodhwankhede1996@gmail.com>
2018-01-14 15:31:51 +0530

    added function to convert string into integer/

--- cpp/MultiplyShiftAdd.cc
+++ cpp/MultiplyShiftAdd.cc
@@ -1,61 +1,30 @@
-// Copyright (c) 2013 Elements of Programming Interviews. All rights reserved.
+/*Problem 6.9:  Write afunction that takes two strings representing integers,
+and returns an integer representing their product.*/
 
-#include <cassert>
 #include <iostream>
-#include <random>
-#include <string>
+using namespace std;
 
-using std::cout;
-using std::default_random_engine;
-using std::endl;
-using std::random_device;
-using std::string;
-using std::uniform_int_distribution;
-
-unsigned Add(unsigned a, unsigned b);
-
-// @include
-unsigned Multiply(unsigned x, unsigned y) {
-  unsigned sum = 0;
-  while (x) {
-    // Examines each bit of x.
-    if (x & 1) {
-      sum = Add(sum, y);
-    }
-    x >>= 1, y <<= 1;
+//converts string to integer
+int convert_to_int(string s1)
+{
+  int n = 0;
+  for(int i=0; i<s1.length(); i++)
+  {
+    n *= 10;
+    n += s1[i] - '0';
   }
-  return sum;
+  return n;
 }
-
-unsigned Add(unsigned a, unsigned b) {
-  unsigned sum = 0, carryin = 0, k = 1, temp_a = a, temp_b = b;
-  while (temp_a || temp_b) {
-    unsigned ak = a & k, bk = b & k;
-    unsigned carryout = (ak & bk) | (ak & carryin) | (bk & carryin);
-    sum |= (ak ^ bk ^ carryin);
-    carryin = carryout << 1, k <<= 1, temp_a >>= 1, temp_b >>= 1;
-  }
-  return sum | carryin;
+int product(string s1, string s2)
+{
+  int n1 = convert_to_int(s1);
+  int n2 = convert_to_int(s2);
+  return n1*n2;
 }
-// @exclude
-
-int main(int argc, char* argv[]) {
-  if (argc == 3) {
-    unsigned int x = atoi(argv[1]), y = atoi(argv[2]);
-    unsigned int res = Multiply(x, y);
-    assert(res == x * y);
-    cout << "PASS: x = " << x << ", y = " << y << "; prod = " << res << endl;
-  } else {
-    default_random_engine gen((random_device())());
-    // Random test, only works if the product is not greater than 2^32 - 1.
-    for (int i = 0; i < 100000; ++i) {
-      uniform_int_distribution<int> dis(0, 65534);
-      unsigned int x = dis(gen), y = dis(gen);
-      unsigned int prod = Multiply(x, y);
-      assert(prod == x * y);
-      cout << "PASS: x = " << x << ", y = " << y << "; prod = " << prod
-           << endl;
-    }
-  }
+int main()
+{
+  string s1 = "324";
+  string s2 = "22";
+  cout<<s1<<" * "<<s2<<" = "<<product(s1, s2)<<endl;
   return 0;
 }
